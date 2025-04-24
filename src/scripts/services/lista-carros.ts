91405de0a03fd29e9949f2bc980572ea5576ff4c
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-24 10:23:51 -0300

    feat: :sparkles: add campo motor

--- src/scripts/services/lista-carros.ts
+++ src/scripts/services/lista-carros.ts
@@ -1,234 +1,311 @@
 const listaCarros = [
   {
     carro: 'byd dolphin mini',
+    motor: 'elétrico',
   },
   {
     carro: 'byd dolphin',
+    motor: 'elétrico',
   },
   {
     carro: 'byd dolphin plus',
+    motor: 'elétrico',
   },
   {
     carro: 'byd han',
+    motor: 'elétrico',
   },
   {
     carro: 'byd seal',
+    motor: 'elétrico',
   },
   {
     carro: 'byd tan',
+    motor: 'elétrico',
   },
   {
     carro: 'byd yuan plus',
+    motor: 'elétrico',
   },
   {
     carro: 'byd yuan pro',
+    motor: 'elétrico',
   },
   {
     carro: 'hyundai ioniq 5',
+    motor: 'elétrico',
   },
   {
     carro: 'kia ev9',
+    motor: 'elétrico',
   },
   {
     carro: 'kia ev5',
+    motor: 'elétrico',
   },
   {
     carro: 'gwm ora 3 skin',
+    motor: 'elétrico',
   },
   {
     carro: 'gwm ora 3 gt',
+    motor: 'elétrico',
   },
   {
     carro: 'neta aya',
+    motor: 'elétrico',
   },
   {
     carro: 'neta x',
+    motor: 'elétrico',
   },
   {
     carro: 'caoachery icar',
+    motor: 'elétrico',
   },
   {
     carro: 'fiat 500e',
+    motor: 'elétrico',
   },
   {
     carro: 'volkswagen id.4',
+    motor: 'elétrico',
   },
   {
     carro: 'volkswagen id.buzz',
+    motor: 'elétrico',
   },
   {
     carro: 'renault kwid e-tech',
+    motor: 'elétrico',
   },
   {
     carro: 'renault megane e-tech',
+    motor: 'elétrico',
   },
   {
     carro: 'renault kangoo e-tech',
+    motor: 'elétrico',
   },
   {
     carro: 'peugeot e-2008',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw ix',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw ix m60',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw i7',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw i4 gran coupe',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw i4 m50 xdrive gran coupe',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw ix3',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw ix2',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw ix1',
+    motor: 'elétrico',
   },
   {
     carro: 'bmw i5 m60',
+    motor: 'elétrico',
   },
   {
     carro: 'audi q6 e-tron',
+    motor: 'elétrico',
   },
   {
     carro: 'audi q8 e-tron',
+    motor: 'elétrico',
   },
   {
     carro: 'audi q8 sportback e-tron',
+    motor: 'elétrico',
   },
   {
     carro: 'audi sq8 sportback e-tron',
+    motor: 'elétrico',
   },
   {
     carro: 'audi e-tron gt quattro',
+    motor: 'elétrico',
   },
   {
     carro: 'audi rs e-tron gt',
+    motor: 'elétrico',
   },
   {
     carro: 'mercedes-benz eqe sedan',
+    motor: 'elétrico',
   },
   {
     carro: 'mercedes-benz eqa',
+    motor: 'elétrico',
   },
   {
     carro: 'mercedes-benz eqb',
+    motor: 'elétrico',
   },
   {
     carro: 'mercedes-benz eqe suv',
+    motor: 'elétrico',
   },
   {
     carro: 'mercedes-benz eqs suv',
+    motor: 'elétrico',
   },
   {
     carro: 'mercedes-benz amg eqs sedan',
+    motor: 'elétrico',
   },
   {
     carro: 'mercedes-benz amg eqe suv',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan 4',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan 4s',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan gts',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan turbo',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan turbo s',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan turbo gt com pacote weissach',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan 4 cross turismo',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan 4s cross turismo',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche taycan turbo cross turismo',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche macan eletric',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche macan 4 eletric',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche macan 4s eletric',
+    motor: 'elétrico',
   },
   {
     carro: 'porsche macan turbo eletric',
+    motor: 'elétrico',
   },
   {
     carro: 'volvo ex90',
+    motor: 'elétrico',
   },
   {
     carro: 'volvo ex40',
+    motor: 'elétrico',
   },
   {
     carro: 'volvo ex30',
+    motor: 'elétrico',
   },
   {
     carro: 'volvo ec40',
+    motor: 'elétrico',
   },
   {
     carro: 'ford mustang mach-e',
+    motor: 'elétrico',
   },
   {
     carro: 'ford e-transit chassi',
+    motor: 'elétrico',
   },
   {
     carro: 'ford e-transit furgao',
+    motor: 'elétrico',
   },
   {
     carro: 'chevrolet equinox ev',
+    motor: 'elétrico',
   },
   {
     carro: 'chevrolet blazer ev',
+    motor: 'elétrico',
   },
   {
     carro: 'jac e-js1 city cargo',
+    motor: 'elétrico',
   },
   {
     carro: 'jac e-jv5.5',
+    motor: 'elétrico',
   },
   {
     carro: 'jac e-jv7l',
+    motor: 'elétrico',
   },
   {
     carro: 'jac e-jv12',
+    motor: 'elétrico',
   },
   {
     carro: 'jac iev-330p',
+    motor: 'elétrico',
   },
   {
     carro: 'jac e-jvcc',
+    motor: 'elétrico',
   },
   {
     carro: 'jac iev1200t',
+    motor: 'elétrico',
   },
   {
     carro: 'jac e-jt9,5',
+    motor: 'elétrico',
   },
   {
     carro: 'jac e-jt12,5',
+    motor: 'elétrico',
   },
 ];
 
