0407d3eda0f669832807261d488bb574ffe46f84
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-24 12:47:48 -0300

    feat: :sparkles: add: esqueleto dos itens de comparação para serem impressos nos cards

--- dist/scripts/components/preencherInfoDoCardDoCarros.js
+++ dist/scripts/components/preencherInfoDoCardDoCarros.js
@@ -1,10 +1,22 @@
-import ehDeQualCard from "../utils/ehDeQualCard.js";
+import ehDeQualCard from '../utils/ehDeQualCard.js';
 function preencherInfoDoCardDoCarro(sugestao, carro) {
     const infoDeQualCarro = ehDeQualCard(carro);
     if (infoDeQualCarro) {
         const infoDoCarroNoInput = infoDeQualCarro.querySelector('.plus');
         if (infoDoCarroNoInput) {
-            infoDoCarroNoInput.textContent = `Motor: ${sugestao.motor}`;
+            infoDoCarroNoInput.textContent = `
+      Motor: ${sugestao.motor}
+      Autonomia Inmetro: ${sugestao.autonomia_inmetro}
+      Torque em Newton-metro: ${sugestao.torque_nm}
+      Torque em quilograma-força por metro: ${sugestao.torque_kgfm}
+      Peso: ${sugestao.peso}
+      Velocidade máxima: ${sugestao.velocidade_maxima}
+      Carregador rápido: ${sugestao.carregador_rapido_suportado}
+      Tempo de carregamento: ${sugestao.tempo_de_carregamento}
+      Capacidade de bateria: ${sugestao.bateria}
+      Potência: ${sugestao.potencia}
+      Preço tabela fipe: ${sugestao.preco_tabela_fipe}
+      `;
         }
     }
 }
