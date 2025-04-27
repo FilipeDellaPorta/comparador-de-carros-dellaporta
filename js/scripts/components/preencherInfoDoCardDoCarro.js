d2bfe530a8a23fb07851528a44ddaddf0f10c4c3
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-04-26 21:17:06 -0300

    fix: :poop: melhorado a visualização das especificações dos carros

--- js/scripts/components/preencherInfoDoCardDoCarro.js
+++ js/scripts/components/preencherInfoDoCardDoCarro.js
@@ -4,16 +4,16 @@ function preencherInfoDoCardDoCarro(sugestao, carro) {
     const infoCarro = infoDeQualCarro.querySelector('.info-carro');
     infoCarro.textContent = '';
     infoCarro.innerHTML = `
-      Motor: ${sugestao.motor} <br>
-      Autonomia Inmetro: ${sugestao.autonomia_inmetro} <br>
-      Torque em Newton-metro: ${sugestao.torque_nm} <br>
-      Torque em quilograma-força por metro: ${sugestao.torque_kgfm} <br>
-      Velocidade máxima: ${sugestao.velocidade_maxima} <br>
-      Tempo da recarga AC: ${sugestao.tempo_de_carregamento_ac} <br>
-      Tempo da recarga mais rápida DC: ${sugestao.tempo_de_carregamento_dc} <br>
-      Capacidade de bateria: ${sugestao.bateria} <br>
-      Potência: ${sugestao.potencia} <br>
-      Preço no site: ${sugestao.preco_site} <br>
+      Motor: <span class="specs-carro">${sugestao.motor}</span> <br>
+      Autonomia Inmetro: <span class="specs-carro">${sugestao.autonomia_inmetro}</span> <br>
+      Torque em Newton-metro: <span class="specs-carro">${sugestao.torque_nm}</span> <br>
+      Torque em quilograma-força por metro: <span class="specs-carro">${sugestao.torque_kgfm}</span> <br>
+      Velocidade máxima: <span class="specs-carro">${sugestao.velocidade_maxima}</span> <br>
+      Tempo da recarga AC: <span class="specs-carro">${sugestao.tempo_de_carregamento_ac}</span> <br>
+      Tempo da recarga mais rápida DC: <span class="specs-carro">${sugestao.tempo_de_carregamento_dc}</span> <br>
+      Capacidade de bateria: <span class="specs-carro">${sugestao.bateria}</span> <br>
+      Potência: <span class="specs-carro">${sugestao.potencia}</span> <br>
+      Preço no site: <span class="specs-carro">${sugestao.preco_site}</span> <br>
       Link: <a href="${sugestao.link}" target="_blank" rel="noopener noreferrer" class="link-carro">${sugestao.carro}</a>
       `;
 }
